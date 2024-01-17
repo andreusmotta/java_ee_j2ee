95b01f6d6ba0ad60059e973b424f5c4317583639
Andreus <andreus.motta@gmail.com>
2024-01-17 10:36:28 -0300

    Aulas criando a agenda, redirecionando o main para o agenda.jsp

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -6,35 +6,44 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+// Importei a classe DAO:
+import model.DAO;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = { "/Controller", "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	// Criando o objeto DAO:
+	DAO dao = new DAO();
+
+	public Controller() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+//		response.getWriter().append("Served at: ").append(request.getContextPath());		
+		// Meu teste de conexão do banco de dados:
+//		dao.testeConexao();
+
+		// Direcionando pro main:
+		String action = request.getServletPath();
+		System.out.println(action);
+		if (action.equals("/main")) {
+			contatos(request, response);
+		}
+	}
+
+	// Listar contatos:
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -1,5 +1,36 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/** Módulo de Conexão **/
+	// Parametros de conexão:
+	private String driver = "org.mariadb.jdbc.Driver";
+	private String url = "jdbc:mariadb://127.0.0.1:3306/dbagenda?useTimezone-true$serverTimezone=UTC";
+	private String user = "root";
+	private String password = "";
+	
+	// Método de conexão:
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con  = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			 System.out.println(e);
+			 return null;
+		}
+	}
+		// teste de conexão
+//		public void testeConexao() {
+//			try {
+//				Connection con = conectar();
+//				System.out.println(con);
+//				con.close();
+//			} catch (Exception e) {
+//				System.out.println(e);
+//			}
+//		}
 }
